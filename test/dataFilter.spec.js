f4b39ba8f4db8f36ec6a91e77608a1f33f33957b
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-06-14 22:52:31 +0530

    json mocking

---
+++ test/dataFilter.spec.js
@@ -0,0 +1,62 @@
+import sinon from 'sinon';
+import {Api, filterByTypeAndSubject} from '../src/dataFilter';
+import {filterByType} from "../src/dataFilter";
+import {filterBySubject} from "../src/dataFilter";
+import {getAPI} from "../src/dataFilter";
+
+let chai = require('chai');
+let expect = chai.expect;
+
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised;
+let data;
+
+describe('Filtering data from json', () => {
+    before = () => {
+        data = [{name: 'Rutherford Scattering', type: 'apps', subject: 'physics'},
+            {name: 'The Ramp', type: 'apps', subject: 'physics'},
+            {name: 'Energy stake Park', type: 'apps', subject: 'chemistry'},
+            {name: 'Pulleys', subject: 'physics', type: 'apps'},
+            {name: 'Masses & Springs', type: 'apps', subject: 'physics'}];
+    };
+
+
+    it('should filter by the given type', () => {
+        let apiMock = sinon.mock(Api);
+        apiMock.expects('getAPI').withArgs('https://raw.githubusercontent.com/balaswecha/pencilbox-2/master/app/json/all.json').returns(Promise.resolve(data));
+
+
+        expect(filterByType('apps')).to.eventually.deep.equal(data);
+
+        apiMock.restore();
+        apiMock.verify();
+    });
+
+    it('should filter by the given subject', () => {
+        let apiMock = sinon.mock(Api);
+        apiMock.expects('getAPI').withArgs('https://raw.githubusercontent.com/balaswecha/pencilbox-2/master/app/json/all.json').returns(Promise.resolve(data));
+
+        const dataWithSubject = [{name: 'Energy stake Park', type: 'apps', subject: 'chemistry'}];
+
+        expect(filterBySubject('chemistry')).to.eventually.deep.equal(dataWithSubject);
+
+        apiMock.restore();
+        apiMock.verify();
+    });
+
+    it('should filter by the given title and subject', () => {
+        let apiMock = sinon.mock(Api);
+        apiMock.expects('getAPI').withArgs('https://raw.githubusercontent.com/balaswecha/pencilbox-2/master/app/json/all.json').returns(Promise.resolve(data));
+
+        const dataWithSubject = [{name: 'Rutherford Scattering', type: 'apps', subject: 'physics'},
+            {name: 'The Ramp', type: 'apps', subject: 'physics'},
+            {name: 'Pulleys', subject: 'physics', type: 'apps'},
+            {name: 'Masses & Springs', type: 'apps', subject: 'physics'}];
+
+        expect(filterByTypeAndSubject('apps', 'physics')).to.eventually.deep.equal(dataWithSubject);
+
+        apiMock.restore();
+        apiMock.verify();
+    });
+
+});
